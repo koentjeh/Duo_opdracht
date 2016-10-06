5822b988d625153aef7cd204e6fadae1b44c231f
koentjeh <koen-debont@hotmail.com>
2016-10-06 10:44:51 +0200

    dingetje toegevoegd

--- website/index.php
+++ website/index.php
@@ -1,13 +1,10 @@
 <?php include 'includes/header.php'; ?>
 
-<div class="container">
-	<div class="col-md-12 card">
-		<h1>De titel</h1>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt eius voluptates laborum commodi laboriosam earum atque nemo vel! Dolor natus animi quod commodi expedita voluptatibus veniam ad in cum aspernatur?</p>
+<div class="container card">
+	<div class="col-sm-12">
+		<h1>De duo opdracht</h1>
+		<p>Deze website is gericht om een helder beeld te geven wat de opleiding Informatica inhoudt. Hierin willen wij U vertellen wat het betekend om op het HBO te Avans Hogeschool te studeren, en wat onze ervaringen zijn om op deze opleiding te studeren.</p>
 	</div>
 </div>
-
-
-<!-- <svg xmlns="http://www.w3.org/2000/svg" width="500" height="500" viewBox="0 0 500 500" preserveAspectRatio="none" style="display: none; visibility: hidden; position: absolute; top: -100%; left: -100%;"><defs><style type="text/css"></style></defs><text x="0" y="25" style="font-weight:bold;font-size:25pt;font-family:Arial, Helvetica, Open Sans, sans-serif">500x500</text></svg></body> -->
 
 <?php include 'includes/footer.php'; ?>
