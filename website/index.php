b909d83899b4425bbc7d85278c25f7d17ea4da74
koentjeh <koen-debont@hotmail.com>
2016-10-06 11:15:21 +0200

    kleine aanpassingen m.b.t. design

--- website/index.php
+++ website/index.php
@@ -1,17 +1,11 @@
 <?php include 'includes/header.php'; ?>
 
-<<<<<<< HEAD
 <div class="container">
-	<div class="col-md-12 card">
-		<h1>De titel</h1>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt eius voluptates laborum commodi laboriosam earum atque nemo vel! Dolor natus animi quod commodi expedita voluptatibus veniam ad in cum aspernatur?</p>
-	
-=======
-<div class="container card">
 	<div class="col-sm-12">
-		<h1>De duo opdracht</h1>
-		<p>Deze website is gericht om een helder beeld te geven wat de opleiding Informatica inhoudt. Hierin willen wij U vertellen wat het betekend om op het HBO te Avans Hogeschool te studeren, en wat onze ervaringen zijn om op deze opleiding te studeren.</p>
->>>>>>> origin/master
+		<div class="card">
+			<h1>De duo opdracht</h1>
+			<p>Deze website is gericht om een helder beeld te geven wat de opleiding Informatica inhoudt. Hierin willen wij U vertellen wat het betekend om op het HBO te Avans Hogeschool te studeren, en wat onze ervaringen zijn om op deze opleiding te studeren.</p>
+		</div>
 	</div>
 </div>
 
--- website/verwachtingen.php
+++ website/verwachtingen.php
@@ -1,8 +1,10 @@
 <?php include 'includes/header.php'; ?>
 
 <div class="container">
-	<div class="col-md-12 card">
-		<h1>Verwactingen</h1>
+	<div class="col-md-12">
+		<div class="card">
+			<h1>Verwactingen</h1>
+		</div>
 	</div>
 </div>
 
