25763197326d817b2e11044f7e25ad02f521f59c
koentjeh <koen-debont@hotmail.com>
2016-10-14 17:06:31 +0200

    lees meer knop toegevoegd op homepagina

--- website/index.php
+++ website/index.php
@@ -5,6 +5,8 @@
 		<div class="card">
 			<h1>De duo opdracht</h1>
 			<p>Deze website is gericht om een helder beeld te geven wat de opleiding Informatica inhoudt. Hierin willen wij U vertellen wat het betekend om op het HBO te Avans Hogeschool te studeren, en wat onze ervaringen zijn om op deze opleiding te studeren.</p>
+
+			<a href="informatica.php"><button class="btn btn-primary btn-sm">Lees meer</button></a>
 		</div>
 	</div>
 </div>
