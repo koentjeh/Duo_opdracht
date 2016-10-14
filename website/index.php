db21452cfa89e7564f8c95051e45795d49904738
regilioDielemans <regilio_dielemans@hotmail.com>
2016-10-14 17:51:20 +0200

    informatica

--- website/index.php
+++ website/index.php
@@ -8,6 +8,10 @@
 
 			<a href="informatica.php"><button class="btn btn-primary btn-sm">Lees meer</button></a>
 		</div>
+		<div class="card">
+			<h1>Informatica</h1>
+			<p>De laatste jaren word er steeds meer gebruik gemaakt van informatica. Bijna iedereen gebruikt dagelijks wel iets wat met informatica te maken heeft. Denk bijvoorbeeld aan WhatsApp, Facebook, Spotify of een nieuwswebsite waar je even snel het laatste nieuws bekijkt. Bij informatica leer je niet alleen het programmeren van een programma. Maar vooral hoe je de eisen van een opdracht gever kan vertalen naar een ontwerp, en die weer kan omzetten naar een werkend computerprogramma en leer je hoe je dit programma goed kan testen. Hiervoor leer je eerst hoe je de eisen van een opdrachtgever goed kan vaststellen. </p>
+		</div>
 	</div>
 </div>
 
